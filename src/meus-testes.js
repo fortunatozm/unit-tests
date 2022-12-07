d6a2574770cafdd135b166d74804d5fcca894083
fortunatozm <fortunatompongo@hotmail.com>
2022-12-07 19:51:33 -0300

    Ajustes finais

--- src/meus-testes.js
+++
@@ -1,6 +0,0 @@
-const svetor = (vacumulator, vcurrent) => vacumulator + vcurrent;
-const average = (vetor) => Math.round(vetor.reduce(svetor) / vetor.length);
-
-let vector = [1, 2, 3, 4];
-
-console.log(average(vector));
